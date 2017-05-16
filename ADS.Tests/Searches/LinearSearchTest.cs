32c011b8384715d3202d89213eee24b6a0201eb9
Nodul <kamil.aniszkiewicz@gmail.com>
2017-05-17 00:08:31 +0200

    Linear Search

---
+++ ADS.Tests/Searches/LinearSearchTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ADS.Searches;
+
+namespace ADS.Tests.Searches
+{
+    [TestClass]
+    public class LinearSearchTest
+    {
+        [TestMethod]
+        public void LinearSearchFindIndexOfIntArray_Valid_True()
+        {
+            // Assign
+            int[] input = {10,50,5,59,600,30,80,0 };
+            int target = 600;
+            int expected = 4; //5 element so 4 for a 0-based array
+            // Act
+            int result = LinearSearch.FindIndexOf(input, target);
+
+            // Assert
+            Assert.AreEqual(expected,result);
+
+        }
+
+        [TestMethod]
+        public void LinearSearchFindIndexOfIntArray_ValidOnly1Element_True()
+        {
+            // Assign
+            int[] input = { 10 };
+            int target = 10;
+            int expected = 0; 
+            // Act
+            int result = LinearSearch.FindIndexOf(input, target);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        public void LinearSearchFindIndexOfIntArray_ElementDoesntExist_Minus1()
+        {
+            // Assign
+            int[] input = { 10, 50, 5, 59, 600, 30, 80, 0 };
+            int target = -500;
+            int expected = -1; 
+            // Act
+            int result = LinearSearch.FindIndexOf(input, target);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void LinearSearchFindIndexOfIntArray_ArrayLengthLessThan1_ArgumentException()
+        {
+            int[] input = {};
+            int target = 600;
+
+            int result = LinearSearch.FindIndexOf(input, target);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void LinearSearchFindIndexOfIntArray_NullInputArray_ArgumentException()
+        {
+            int[] input = null;
+            int target = 600;
+
+            int result = LinearSearch.FindIndexOf(input, target);
+        }
+
+    }
+}
---
+++ ADS/Searches/LinearSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADS.Searches
+{
+    // Simply iterate through all elements till you find the one you want. If you checked all, return null or -1
+    // Also known as sequential search
+
+    public static class LinearSearch
+    {
+        /// <summary>
+        /// Returns index of the target element in the input array. Returns -1 if not found
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int FindIndexOf(int[] input, int target)
+        {
+            if (input == null) throw new ArgumentNullException("Input array reference cannot be null");
+            if (input.Length < 1) throw new ArgumentException("Length of input array cannot be shorter than 1");
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == target) return i;
+            }
+            return -1; // target not found in input collection
+        }
+    }
+}
